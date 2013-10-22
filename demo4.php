37863c3e76a4be11e8f7091921d5d218492db432
Asad Saqib <asadsaqib@fas.harvard.edu>
2013-10-21 22:09:53 -0400

    first commit

---
+++ demo4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<?php
+	require_once('logic.php');
+	?>
+
+	<link rel='stylesheet' href='styles.css' type='text/css'>
+
+</head>
+
+<body>
+
+	<div class = 'card'></div>
+
+</body>
+</html>
