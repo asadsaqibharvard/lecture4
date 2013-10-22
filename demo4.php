b9ff0e556a1ab54f10bb179dc4ce5e7d7ca5cf8e
Asad Saqib <asadsaqib@fas.harvard.edu>
2013-10-21 23:08:12 -0400

    box assignment random size

--- demo4.php
+++ demo4.php
@@ -6,16 +6,26 @@
 	require_once('logic.php');
 	?>
 
+	<style>
+		.box {
+			width: 50px;
+			height: 50px;
+			float: left;
+			margin: 4px;
+			background-color: red;
+		}
+	</style>
+	
+	<!--
 	<link rel='stylesheet' href='styles.css' type='text/css'>
+	-->
 
 </head>
 
 <body>
 
-	<div class = 'card'>1</div>
-	<div class = 'card'>2</div>
-	<div class = 'card'>3</div>
-	<div class = 'card'>4</div>
+	<?=$boxes?>
+	
 
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,8 +1,17 @@
 <?php
 $boxes = "";
 for($i = 0; $i < 10; $i++){
-	echo "do loop<br>";
-	//$boxes = $boxes
+	//echo $i.' do loop<br>';
+	//$boxes = $boxes."<div class='card'>".$i."</div>";
+
+
+	$w = rand(100, 500);
+	$h = rand(100, 500);
+	$boxes = $boxes."<div class='box' style='width: ".$w."px; height:".$h."px;'>".$i."</div>";
 }
+
+
+/*rand, inline css, php talking to css, different box width every time*/
+
 
 ?>
